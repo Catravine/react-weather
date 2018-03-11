4a3f0b9a978d39003400a128de066b4322ba757f
Caroline Courtney <caroline@carolinecourtney.com>
2018-03-11 17:39:39 -0400

    Charts and Maps, third party libraries added in

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // concat also returns a new array, so it won't actually mutate state
+            // push mutates array so don't use that.
+            return [ action.payload.data, ...state ];
+    }
     return state;
 }
